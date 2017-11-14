b1d81a8cdcf839225fd204ad5457265c5dfdffa3
HWANGJUNGWOO <ghkdwnddn@hanmail.net>
2017-11-15 00:17:31 +0900

    Resource

---
+++ Rooms/Assets/LockInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockInput : MonoBehaviour {
+    public GameObject lockpassword;
+    public GameObject InputKey;
+    public void ClickInput()
+    {
+        InputKey.transform.localScale=new Vector3(1.2f, 0.9f, 0.26728f);
+        lockpassword.transform.position = new Vector3(93.43f, 10.51f, 29.8f);
+        lockpassword.transform.localRotation=new Quaternion (0.0f, 129.0f, 0.0f,60.0f);
+        lockpassword.transform.localScale = new Vector3(0.2183667f, 0.7f, 0.7f);
+    }
+    public void ClickExit()
+    {
+        InputKey.transform.localScale = new Vector3(1.0f, 0.55f, 0.26728f);
+    }
+}
---
+++ Rooms/Assets/LockOff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOff : MonoBehaviour {
+    public GameObject lockpassword;
+    public GameObject OffKey;
+    public void ClickOff()
+    {
+        OffKey.transform.localScale = new Vector3(1.2f, 0.9f, 0.26728f);
+        lockpassword.transform.position = new Vector3(93.43f, 10.51f, 32.7f);
+        lockpassword.transform.localRotation = new Quaternion(0.0f, 90.0f, 0.0f, 90.0f);
+        lockpassword.transform.localScale = new Vector3(0.2183667f, 0.55f, 0.52f);
+    }
+    public void ClickExit()
+    {
+        OffKey.transform.localScale = new Vector3(1.0f, 0.55f, 0.26728f);
+    }
+}
